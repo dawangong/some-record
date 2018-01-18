ecf649b1b82b054dd90c99247b6e892b0f50a1d1
dawangong <dawangong@126.com>
2018-01-18 20:30:21 +0800

    update

---
+++ some-test/es6-class.js
@@ -0,0 +1,23 @@
+class People {
+    constructor () {
+
+    }
+    speak() {
+        console.log('你好');
+    }
+}
+
+let dav = new People();
+dav.speak();
+
+class blackPeople extends People{
+    constructor () {
+        super();
+    }
+    go () {
+        console.log('走');
+    }
+}
+let wxm = new blackPeople();
+wxm.go();
+wxm.speak();
