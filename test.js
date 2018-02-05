e224beaaf01e30f68f30a39a8d4c0a49fd1c76a9
dawangong <dawangong@126.com>
2018-02-05 12:13:28 +0800

    question

---
+++ test.js
@@ -0,0 +1,16 @@
+var arr =[1,2,3];
+
+function change (arr) {
+	console.log(arr);
+	arr = [];
+	console.log(arr);
+}
+
+change(arr);
+
+console.log(arr);
+
+// var a = [1,2,3];
+// var b = a;
+// a = [4,5,6];
+// console.log(b);
