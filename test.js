1ddd869ac5c5f3ae6b5ee22a7da6581022eeddb6
dawangong <dawangong@126.com>
2018-02-05 17:18:03 +0800

    update

--- test.js
+++ test.js
@@ -1,16 +1,30 @@
-var arr =[1,2,3];
+// let arr =[1,2,3];
 
-function change (arr) {
-	console.log(arr);
-	arr = [];
-	console.log(arr);
-}
+// function change (arr) {
+// 	console.log(arr);
+// 	arr = [];
+// 	console.log(arr);
+// }
 
-change(arr);
+// change(arr);
 
-console.log(arr);
+// console.log(arr);
 
 // var a = [1,2,3];
 // var b = a;
 // a = [4,5,6];
 // console.log(b);
+
+function group(array, subGroupLength) {
+		let index = 0;
+		const newArray = [];
+
+		while (index < array.length) {
+			newArray.push(array.slice(index, index += subGroupLength));
+		}
+		return newArray;
+	}
+
+
+var arr = group([1,2,3,4,5],2);
+console.log(arr);
