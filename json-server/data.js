146f866bcbeb39ed66766ff5e2f3b6fe5ed06ca9
dawangong <dawangong@126.com>
2018-02-26 16:03:48 +0800

    json-server的学习

--- json-server/data.js
+++ json-server/data.js
@@ -1,25 +1,10 @@
-// import Mock from 'mockjs';
-let Mock=require('mockjs');
-let Random=Mock.Random;
-
-module.exports=()=>{
-	let data={
-		news:[]
-	};
-
-	let images=[1,2,3].map(x=>Random.image('120x60',Random.color(),Random.word(2,6)));
-
-	for(let i=0;i<100;i++){
-		let content=Random.cparagraph(0,10);
-
-		data.news.push({
-			id:i,
-			title:Random.cword(8,20),
-			desc:content.substr(0,40),
-			tag:Random.cword(2,6),
-			views:Random.integer(100,5000),
-			images:images.slice(0,Random.integer(1,3))
-		})
+module.exports = () => {
+	const data = { users: [] };
+	// Create 1000 users
+	for (let i = 0; i < 1000; i++) {
+		data.users.push({ id: i, name: `user${i}` })
 	}
-	return data
+	return data;
 };
+
+//使用json-server本身模拟数据
