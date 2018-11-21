02ee1f579d7c02700da8560b1120a3552c4520ba
whq <dawangong@126.com>
2018-11-21 16:51:42 +0800

    装饰器

--- decorator/decorator.js
+++ decorator/decorator.js
@@ -1,12 +1,72 @@
-function f(target) {
-    target.prop = '2333';
-}
-
+// 无参数修饰类
 @f
 class Test {
 
 }
 
+function f(target) {
+    target.prototype.prop = '2333';
+}
+
 let test = new Test();
 
 console.log(test.prop);
+
+// 带参数修饰类
+@f1('111')
+class Test1 {
+
+}
+
+function f1(str) {
+    return (target) => {
+        target.prototype.go = () => {
+            console.log(str);
+        }
+    }
+}
+
+let test1 = new Test1();
+
+test1.go();
+
+// 无参数修饰方法
+class Person {
+    @f2
+    speak() {
+        console.log(1);
+    }
+}
+
+function f2(target, name, descriptor) {
+    const temp = descriptor.value;
+    descriptor.value = () => {
+        temp();
+        console.log(2);
+    }
+}
+
+let ll = new Person();
+
+ll.speak();
+
+// 带参数修饰方法
+class Person1 {
+    @f3('???')
+    speak() {
+        console.log(1);
+    }
+}
+
+function f3(str) {
+    return (target, name, descriptor) => {
+        const temp = descriptor.value;
+        descriptor.value = () => {
+            temp();
+            console.log(str);
+        }
+    }
+}
+
+let R = new Person1();
+R.speak();
