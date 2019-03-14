6896d59e8d2732d42e3765bf6bfb7fec9b15dd1e
whq <dawangong@126.com>
2019-03-14 22:53:35 +0800

    some和every美化判断

---
+++ 美化判断/多且判断/result.js
@@ -0,0 +1,6 @@
+const a = '23333';
+const compare = ['33333', '43333', '53333', '63333'];
+
+if (compare.every(item => item === a)) {
+    console.log(1);
+}
--- 美化判断/多或判断/2-result.js
+++ 美化判断/多或判断/2-result.js
@@ -1,6 +1,10 @@
 const a = '23333';
-const compare = ['23333', '33333', '43333', '53333']
+const compare = ['23333', '33333', '43333', '53333'];
 
 if (compare.includes(a)) {
+    console.log(1);
+}
+
+if(compare.some(item => item === a)) {
     console.log(1);
 }
