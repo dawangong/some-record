5c62bbed44543272fbca819baa65eedd546be5d8
齐文豪 <dawangong@126.com>
2020-07-03 15:46:17 +0800

    auto compose

---
+++ hoc/auto-compose.js
@@ -0,0 +1,24 @@
+const add = (a, b) => a + b;
+const double = x => 2 * x;
+const reduce  = x => x - 1;
+
+const autoCompose = (...fn) => {
+  let next;
+  const [firstFn, ...fns] = fn;
+  return (...arg) => {
+    next = firstFn(...arg);
+    let n = 0;
+    while (n < fns.length) {
+      next = fns[n](next);
+      n++;
+    }
+    return next;
+  }
+};
+
+
+const fn = autoCompose(add, double, reduce);
+
+const result = fn(1, 2); // 5
+
+console.log(result);
