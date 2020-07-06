943237fa0a6b78a7e74a17275a616d1a9c241d0a
齐文豪 <dawangong@126.com>
2020-07-06 10:45:47 +0800

    函数组合 pointfree

--- hoc/auto-compose.js
+++ hoc/auto-compose.js
@@ -2,19 +2,21 @@ const add = (a, b) => a + b;
 const double = x => 2 * x;
 const reduce  = x => x - 1;
 
-const autoCompose = (...fn) => {
-  let next;
-  const [firstFn, ...fns] = fn;
-  return (...arg) => {
-    next = firstFn(...arg);
-    let n = 0;
-    while (n < fns.length) {
-      next = fns[n](next);
-      n++;
-    }
-    return next;
-  }
-};
+// const autoCompose = (...fn) => {
+//   let next;
+//   const [firstFn, ...fns] = fn;
+//   return (...arg) => {
+//     next = firstFn(...arg);
+//     let n = 0;
+//     while (n < fns.length) {
+//       next = fns[n](next);
+//       n++;
+//     }
+//     return next;
+//   }
+// };
+
+const autoCompose = (...fns) => (...args) => fns.reduce((params, fn, index) => index === 0 ? fn(...params) : fn(params), args);
 
 const fn = autoCompose(add, double, reduce);
 
