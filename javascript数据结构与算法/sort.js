c736e19030130abd2c942ed41722c430dc198e88
齐文豪 <dawangong@126.com>
2020-07-27 14:40:47 +0800

    排序算法

---
+++ javascript数据结构与算法/sort.js
@@ -0,0 +1,50 @@
+const arr = [1,4,5,99,2];
+
+const bubble = arr => {
+  if (!arr || arr.length < 2) return arr;
+  for(let i = 0; i< arr.length - 1; i++) {
+    for(let j = 0; j< arr.length - 1 - i; j ++) {
+      if(arr[j] > arr[j+1]) {
+        const temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+  return arr;
+};
+console.log(bubble(arr));;
+
+const quick = arr => {
+  if (!arr || arr.length < 2) return arr;
+  const left = [];
+  const right = [];
+  const base = arr[0];
+  arr.forEach(item => {
+    if(item < base) {
+      left.push(item);
+    }
+    if(item > base) {
+      right.push(item);
+    }
+  });
+  return [...quick(left), base, ...quick(right)];
+};
+console.log(quick(arr));
+
+const select = arr => {
+  let minIndex;
+  for(let i = 0; i< arr.length - 1; i++) {
+    minIndex = i;
+    for(let j = i + 1; j< arr.length - 1 - i; j ++) {
+      if(arr[j] < arr[minIndex]) {
+        const temp = arr[minIndex];
+        arr[minIndex] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(select(arr));
