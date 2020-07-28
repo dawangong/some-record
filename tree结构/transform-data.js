91612002757234039db97a0e80a8dd8a6f43355e
齐文豪 <dawangong@126.com>
2020-07-28 16:07:25 +0800

    语法优化

--- tree结构/transform-data.js
+++ tree结构/transform-data.js
@@ -1,8 +1,8 @@
 const transformData = originData => {
   if (!originData || originData.length === 0) return;
 
-  for (var i = 0; i < originData.length; i++) {
-    for (var j = 0; j < originData.length; j++) {
+  for (let i = 0; i < originData.length; i++) {
+    for (let j = 0; j < originData.length; j++) {
       if (originData[i].id === originData[j].pid) {
         originData[i].childern = originData[i].childern || [];
         originData[i].childern.push(originData[j]);
