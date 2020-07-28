87b6c7a1e29297b42269234847d3db6fa1c2cdf9
齐文豪 <dawangong@126.com>
2020-07-28 16:06:19 +0800

    解构优化

--- javascript数据结构与算法/sort.js
+++ javascript数据结构与算法/sort.js
@@ -5,9 +5,7 @@ const bubble = arr => {
   for(let i = 0; i< arr.length - 1; i++) {
     for(let j = 0; j< arr.length - 1 - i; j ++) {
       if(arr[j] > arr[j+1]) {
-        const temp = arr[j];
-        arr[j] = arr[j+1];
-        arr[j+1] = temp;
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
       }
     }
   }
@@ -38,9 +36,7 @@ const select = arr => {
     minIndex = i;
     for(let j = i + 1; j< arr.length - 1 - i; j ++) {
       if(arr[j] < arr[minIndex]) {
-        const temp = arr[minIndex];
-        arr[minIndex] = arr[j];
-        arr[j] = temp;
+        [arr[j], arr[minIndex]] = [arr[minIndex], arr[j]];
       }
     }
   }
